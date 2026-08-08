30e99c9b5cdce02bf7415c4a2326d99c4019ccf5
Sarvesh Kumar <88645840+Sarvesh-K7@users.noreply.github.com>
2026-08-09 00:33:12 +0530

    Refactor CORS configuration in create_app function

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,7 +18,16 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app, origins=app.config["CORS_ORIGINS"], supports_credentials=True)
+    # CORS(app, origins=app.config["CORS_ORIGINS"], supports_credentials=True)
+    CORS(
+    app,
+    resources={
+        r"/api/*": {
+            "origins": app.config["CORS_ORIGINS"]
+        }
+    },
+    supports_credentials=True,
+)
 
     db.init_app(app)
     with app.app_context():
